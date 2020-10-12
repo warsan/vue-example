341882fb4adf618c5fccd0833c58a1464567616e
Сандро <warsan00@mail.ru>
2020-10-12 08:18:06 +0500

    Правки

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
+    <img width="40px" src="./assets/logo.png">
     <Hello msg="Привет, Vue в CodeSandbox!" />
+    <p>Пример простого кода Vuex, реализующего счётчик:</p>
+    <iframe width="600" height="300vmin" 
+    src="//jsfiddle.net/warsand/20bLnus7/embedded/js,html,css,result/dark/" 
+    allowfullscreen="allowfullscreen" 
+    allowpaymentrequest frameborder="0">
+    </iframe>
   </div>
 </template>
 
