5ec70682d06598d75f96487ccc64a65b071f4bba
Сандро <warsan00@mail.ru>
2020-10-31 14:14:15 +0500

    Правки

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,77 +3,59 @@
     <h1>{{ msg }}</h1>
     <h3>Установленные плагины CLI</h3>
     <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+             target="_blank"
+             rel="noopener">babel</a>
       </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+             target="_blank"
+             rel="noopener">eslint</a>
       </li>
     </ul>
     <h3>Основные ссылки</h3>
     <ul>
-      <li>
-        <a href="https://ru.vuejs.org/index.html" target="_blank" rel="noopener">Основные документы</a>
+      <li><a href="https://ru.vuejs.org/index.html" 
+             target="_blank" 
+             rel="noopener">Основные документы</a>
       </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Форум</a
-        >
+      <li><a href="https://forum.vuejs.org" 
+             target="_blank" 
+             rel="noopener">Форум</a>
       </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Чат сообщества</a
-        >
+      <li><a href="https://chat.vuejs.org" 
+             target="_blank" 
+             rel="noopener">Чат сообщества</a>
       </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Твиттер</a
-        >
+      <li><a href="https://twitter.com/vuejs" 
+      target="_blank" rel="noopener"
+          >Твиттер</a>
       </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">Новости</a>
+      <li><a href="https://news.vuejs.org" 
+             target="_blank" 
+             rel="noopener">Новости</a>
       </li>
     </ul>
     <h3>Экосистема</h3>
     <ul>
-      <li>
-        <a href="https://router.vuejs.org/ru/" target="_blank" rel="noopener"
-          >vue-router</a
-        >
+      <li><a href="https://router.vuejs.org/ru/" 
+             target="_blank" 
+             rel="noopener">vue-router</a>
       </li>
-      <li>
-        <a href="https://vuex.vuejs.org/ru/" target="_blank" rel="noopener">vuex</a>
+      <li><a href="https://vuex.vuejs.org/ru/" 
+             target="_blank" 
+             rel="noopener">vuex</a>
       </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools"
+             target="_blank"
+             rel="noopener">vue-devtools</a>
       </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org/ru/" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
+      <li><a href="https://vue-loader.vuejs.org/ru/" 
+             target="_blank" 
+             rel="noopener">vue-loader</a>
       </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
+      <li><a href="https://github.com/vuejs/awesome-vue"
+             target="_blank"
+             rel="noopener">awesome-vue</a>
       </li>
     </ul>
   </div>
@@ -90,18 +72,12 @@ export default {
 
 <!-- Добавьте атрибут "scoped", чтобы ограничить CSS только этим компонентом -->
 <style scoped>
-h3 {
-  margin: 20px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 5px;
-}
-a {
-  color: #42b983;
-}
+  h3 { margin: 20px 0 0; }
+  a { color: #42b983; }
+  ul { padding: 0;
+     list-style-type: none;
+     }
+  li { margin: 0 5px;
+     display: inline-block;
+     }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import App from "./App.vue";
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App) // выполнить в App
 }).$mount("#app");
+// смонтировать в #app
